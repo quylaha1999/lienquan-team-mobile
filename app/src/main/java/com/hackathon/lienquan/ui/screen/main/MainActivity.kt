e4204f4bea8bd12759360bb9e20a6bf940b8c396
nguyen.van.quangc <SUN-ASTERISK\nguyen.van.quangc@b121544-pc.sun-asterisk.com>
2019-10-18 10:13:58 +0700

    Add home screen

--- app/src/main/java/com/hackathon/lienquan/ui/screen/main/MainActivity.kt
+++ app/src/main/java/com/hackathon/lienquan/ui/screen/main/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.hackathon.lienquan.ui.screen.main
 
+import android.os.Bundle
 import com.hackathon.lienquan.R
 import com.hackathon.lienquan.databinding.ActivityMainBinding
 import com.hackathon.lienquan.ui.base.BaseActivity
+import com.hackathon.lienquan.ui.screen.home.HomeFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     override val viewModel: MainViewModel by viewModel()
     override val layoutId: Int = R.layout.activity_main
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        addFragment(fragment = HomeFragment.newInstance(), TAG = HomeFragment.TAG)
+    }
 }
